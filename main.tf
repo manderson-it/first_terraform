9230b2b2ebf7f15bfc4beea343d196de735259a2
marek <marek.anderson@arctiq.ca>
2021-01-06 21:33:04 +0000

    chg to invalid mtype

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "default" {
   name         = "my-first-instance"
-  machine_type = "f1-micro"
+  machine_type = "e2-standard-2"
   zone         = "us-central1-a"
 
   boot_disk {
